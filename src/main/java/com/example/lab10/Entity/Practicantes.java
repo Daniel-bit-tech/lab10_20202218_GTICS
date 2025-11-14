cdeb20f66dbcd30cebf6a061aad2649f1c38db61
Mona Lisa <tuemail@ejemplo.com>
2025-11-13 21:16:39 -0500

    avance de api

---
+++ src/main/java/com/example/lab10/Entity/Practicantes.java
@@ -0,0 +1,20 @@
+package com.example.lab10.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Practicantes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre_completo;
+    private String carrera;
+    private String universidad;
+    private String email;
+    private String pais;
+    private String estado;
+
+}
---
+++ src/main/java/com/example/lab10/Repository/PracticantesRepository.java
@@ -0,0 +1,8 @@
+package com.example.lab10.Repository;
+
+import com.example.lab10.Entity.Practicantes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PracticantesRepository extends JpaRepository<Practicantes, Long> {
+
+}
